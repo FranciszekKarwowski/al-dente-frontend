a0d13feac50453194314d075a21ba3f0f4105c4c
FranciszekKarwowski <franek.karwowski.9@gmail.com>
2023-03-09 19:42:28 +0100

    add WizytyView

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,7 @@ import { RouterLink, RouterView } from "vue-router";
             <HelloWorld msg="You did it!" />
 
             <nav>
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
+				<RouterLink to="/wizyty">Wizyty</RouterLink>
             </nav>
         </div>
     </header>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <main>
+        dupa home view
+    </main>
+</template>
