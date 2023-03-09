5197a3917bb323381d0c44aa53538b7fd102467d
FranciszekKarwowski <franek.karwowski.9@gmail.com>
2023-03-09 20:29:44 +0100

    add mock list to PacjenciView

--- src/views/PacjenciView.vue
+++ src/views/PacjenciView.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
+    let id = 0;
+    const patients = [
+        { id:id++, name: 'Areczek', surname: 'Mareczek' },
+        { id:id++, name: 'Franek', surname: 'Karwa' },
+        { id:id++, name: 'Tomasz', surname: 'Sztajka' },
+        { id:id++, name: 'Jancja', surname: 'Gontar' },
+    ];
+
 </script>
 
 <template>
     <main>
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-
+        <ul>
+            <li v-for="patient in patients">
+                {{ patient.name }} {{ patient.surname }}
+            </li>
+        </ul>
     </main>
 </template>
